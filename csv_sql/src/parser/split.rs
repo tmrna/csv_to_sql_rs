93e592a599cc48de6949cf906a18790ff022f0e6
tmrna <80976892+tmrna@users.noreply.github.com>
2023-07-25 12:57:30 -0400

    added helper functions for trimming whitespace

--- csv_sql/src/parser/split.rs
+++ csv_sql/src/parser/split.rs
@@ -40,6 +40,8 @@ impl Split {
                 substr.push(ch);
             }
         }
+
+        self.strip_substr_whitespace();
     }
 
     fn handle_punctuation(&mut self, ch: char, substr:&mut String) {
@@ -58,8 +60,83 @@ impl Split {
             }
         }
 
-        else if !self.specials.empty()  && !self.specials.contains('"') && ch != '"'{
+        else if self.specials.contains('"') && ch != '"' {
             substr.push(ch);
         }
+
+        else if !self.specials.contains('"') && ch == '"' {
+
+        } 
+    }
+
+    fn strip_substr_whitespace(&mut self) {
+        let mut new_body = Vec::default();
+        for substr in self.body.clone() {
+            match get_slice(&substr, get_bounds(&substr)) {
+                Some(new_string) => new_body.push(new_string),
+                None => {},
+            }
+        }
+        self.body = new_body;
+    }
+}
+
+////////////////////////////  HELPER //////////////////////////////////
+
+fn get_bounds(substr: &String) -> Option<(usize, usize)> {
+    let mut hit_begin = false;
+    let mut hit_end = false;
+    let mut left = 0;
+    let mut right = substr.len() - 1;
+
+    while left < right && !(hit_begin && hit_end) {
+        if !check_nth_whitespace(&substr, left) && !hit_begin {
+            hit_begin = true;
+        }
+        if !check_nth_whitespace(&substr, right) && !hit_end {
+            hit_end = true;
+        }
+        left += !hit_begin as usize;
+        right -= !hit_end as usize;
+    }
+
+    if left >= right {
+        return None;
+    }
+
+    return Some((left, right));
+}
+
+fn get_nth(substr: &String, index: usize) -> Option<char> {
+    return substr.chars().nth(index);
+}
+
+fn check_nth_whitespace(substr: &String, index: usize) -> bool {
+    match get_nth(&substr, index) {
+
+        Some(ch) => ch.is_whitespace(),
+
+        None => false,
+    }
+}
+
+fn get_slice(substr: &String, bounds: Option<(usize, usize)>) -> Option<String> {
+    match bounds {
+        Some(borders) => {
+
+            let mut res = String::default();
+
+            for i in borders.0 .. borders.1 {
+                match get_nth(&substr, i) {
+
+                    Some(ch) => res.push(ch),
+
+                    None => {return None;},
+                }
+            }
+
+            return Some(res);
+        },
+        None => None,
     }
 }
